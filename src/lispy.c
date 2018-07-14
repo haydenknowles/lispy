5acfe26d7230a467498b301b93ec75fa3cbfa70e
Hayden Knowles <hayden.knowles@live.com>
2018-07-14 13:05:37 +1200

    added ability to negate, refactored

--- src/lispy.c
+++ src/lispy.c
@@ -3,34 +3,8 @@
 
 #include "mpc.h"
 
-long eval_op(long x, char* op, long y) {
-    if (strcmp(op, "+") == 0) { return x + y; }
-    if (strcmp(op, "-") == 0) { return x - y; }
-    if (strcmp(op, "*") == 0) { return x * y; }
-    if (strcmp(op, "/") == 0) { return x / y; }
-    if (strcmp(op, "%") == 0) { return x % y; }
-    if (strcmp(op, "^") == 0) { return pow(x, y); }
-    return 0;
-}
-
-long eval(mpc_ast_t* t) {
-    if (strstr(t->tag, "number")) {
-        return atoi(t->contents);
-    }
-
-    // the operator is always the second child
-    char* op = t->children[1]->contents;
-
-    long x = eval(t->children[2]);
-
-    int i = 3;
-    while (strstr(t->children[i]->tag, "expr")) {
-        x = eval_op(x, op, eval(t->children[i]));
-        i++;
-    }
-
-    return x;
-}
+static long eval_op(long x, char* op, long y);
+static long eval(mpc_ast_t* t);
 
 int main(int argc, char** argv) {
 
@@ -76,5 +50,38 @@ int main(int argc, char** argv) {
 
     mpc_cleanup(4, Number, Operator, Expr, Lispy);
     
+    return 0;
+}
+
+static long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    // the operator is always the second child
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while (strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+
+    if (i == 3 && strcmp(op, "-") == 0) {
+        x = eval_op(0, op, x);
+    }
+
+    return x;
+}
+
+static long eval_op(long x, char* op, long y) {
+    if (strcmp(op, "+") == 0) { return x + y; }
+    if (strcmp(op, "-") == 0) { return x - y; }
+    if (strcmp(op, "*") == 0) { return x * y; }
+    if (strcmp(op, "/") == 0) { return x / y; }
+    if (strcmp(op, "%") == 0) { return x % y; }
+    if (strcmp(op, "^") == 0) { return pow(x, y); }
     return 0;
 }
